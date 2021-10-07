b1a44ccb9e1eeb42368b65504a3b3ecea741b613
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2021-10-07 19:06:01 +0100

    Creating function to produce figure. Adding in vehicle and pedestrian polygons

--- figures/figs_ped_network.py
+++ figures/figs_ped_network.py
@@ -5,11 +5,68 @@ import json
 sys.path.append("..")
 import createPavementNetwork as cpn
 import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 
 with open("figure_config.json") as f:
     fig_config = json.load(f)
+
+
+#################################
+#
+#
+# Functions
+#
+#
+#################################
+def figure_pavement_nodes_for_single_road_node(node_id, gdfPedNodes, gdfORLink, gdfTopoVeh, gdfTopoPed, config = fig_config):
+
+    # Initialise figure
+    f, ax = plt.subplots(1,1, figsize = (10,10))
+
+    # Select the pavement nodes
+    gdfPedNodesPlot = gdfPedNodes.loc[ gdfPedNodes['juncNodeID']==node_id]
+
+    # Get corresponding road links ids
+    rls = pd.concat([gdfPedNodesPlot['v1rlID'], gdfPedNodesPlot['v2rlID']]).unique()
+    
+    # Add pedestrian and vehicle polygons
+    gdfTopoVeh = gdfTopoVeh.loc[ gdfTopoVeh['roadLinkID'].isin(rls)]
+    gdfTopoPed = gdfTopoPed.loc[ gdfTopoPed['roadLinkID'].isin(rls)]
+
+    # Get road links to plot for this node
+    for i, row in gdfPedNodesPlot.head(1).iterrows():
+        # Get road link angles
+        a1 = row['a1']
+        a2 = row['a2']
+
+        road_node = Point([row['juncNodeX'], row['juncNodeY']])
+
+        a1, a2 = cpn.filter_angle_range(a1, a2, cpn.default_angle_range)
+
+        rays = cpn.rays_between_angles(a1, a2, road_node, ray_length = cpn.default_ray_length)
+
+        gdfRays = gpd.GeoDataFrame({'geometry':rays})
+        gdfRLPlot = cpn.gdfORLink.loc[ (cpn.gdfORLink['fid']==row['v1rlID']) | (cpn.gdfORLink['fid']==row['v2rlID'])]
+
+        gdfTopoVeh.plot(ax=ax, color = config['cariageway_facecolour'])
+        gdfTopoPed.plot(ax=ax, color = config['pavement_facecolour'])
+        gdfRLPlot.plot(ax=ax, color='black')
+        gdfRays.plot(ax=ax)
+
+        # for some reason need to specify zorder value for this to appear on top
+        gdfPedNodes.loc[ (gdfPedNodes['v1rlID'] == row['v1rlID']) & (gdfPedNodes['v2rlID'] == row['v2rlID']) ].plot(ax=ax, color = config['pavement_node_facecolour'], zorder=10)
+
+        # Set axis limits
+        boarded_dist = 5
+        xmin, ymin, xmax, ymax = gdfRLPlot.total_bounds
+        ax.set_xlim(xmin-5, xmax+5)
+        ax.set_ylim(ymin-5, ymax+5)
+
+    ax.set_axis_off()
+    return f
+
 
 #################################
 #
@@ -34,33 +91,4 @@ dfPedNodes = cpn.multiple_road_node_pedestrian_nodes_metadata(cpn.G, cpn.gdfORNo
 # set which road node to illustrate getting ped nodes for
 node_id = fig_config['pavenet_config']['or_node_ids'][0]
 
-# Get road links to plot for this node
-dfPedNodesPlot = dfPedNodes.loc[ dfPedNodes['juncNodeID']==node_id]
-gdfPedNodesPlot = gdfPedNodes.loc[ gdfPedNodes['juncNodeID']==node_id]
-
-# I think build up plot from each row
-f, ax = plt.subplots(1,1, figsize = (10,10))
-
-for i, row in gdfPedNodesPlot.head(1).iterrows():
-
-
-	# Get road link angles
-    a1 = row['a1']
-    a2 = row['a2']
-
-    road_node = Point([row['juncNodeX'], row['juncNodeY']])
-
-    a1, a2 = cpn.filter_angle_range(a1, a2, cpn.default_angle_range)
-
-    rays = cpn.rays_between_angles(a1, a2, road_node, ray_length = cpn.default_ray_length)
-
-    gdfRays = gpd.GeoDataFrame({'geometry':rays})
-    cpn.gdfORLink.loc[ (cpn.gdfORLink['fid']==row['v1rlID']) | (cpn.gdfORLink['fid']==row['v2rlID'])].plot(ax=ax, color='black')
-    gdfRays.plot(ax=ax)
-    gdfPedNodesPlot.loc[i:i].plot(ax=ax)
-    gdfPedNodes.loc[ (gdfPedNodes['v1rlID'] == row['v1rlID']) & (gdfPedNodes['v2rlID'] == row['v2rlID']) ].plot(ax=ax)
-
-ax.set_axis_off()
-f.show()
-
-
+f = figure_pavement_nodes_for_single_road_node(node_id, gdfPedNodes, cpn.gdfORLink, cpn.gdfTopoVeh, cpn.gdfTopoPed, config = fig_config)
