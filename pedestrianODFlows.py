955c884e089d302ec67cde66285308bb7e6b6289
Obi Sargoni <othompsarg@hotmail.com>
2022-09-21 10:23:22 +0100

    Allowing flows to be produced without producing ODs. Useful for preserving a previous set of ODs

--- pedestrianODFlows.py
+++ pedestrianODFlows.py
@@ -37,6 +37,8 @@ poi_file = os.path.join(gis_data_dir, config["poi_file"])
 centre_poi_ref = config["centre_poi_ref"]
 dist_from_centre_threshold = 50
 
+create_ods = False
+
 
 #################
 #
@@ -63,62 +65,59 @@ def get_random_point_in_polygon(poly):
 #
 #################
 
-# Select Origin pavement nodes based on POIs
-c = fiona.open(poi_file)
-gdf_pois = gpd.GeoDataFrame.from_features(c)
-if gdf_pois.crs is None:
-    gdf_pois.crs = projectCRS
-else:
-    assert gdf_pois.crs.to_string().lower() == projectCRS
+if create_ods:
+    # Select Origin pavement nodes based on POIs
+    c = fiona.open(poi_file)
+    gdf_pois = gpd.GeoDataFrame.from_features(c)
+    if gdf_pois.crs is None:
+        gdf_pois.crs = projectCRS
+    else:
+        assert gdf_pois.crs.to_string().lower() == projectCRS
 
-gdfPaveNode = gpd.read_file(pavement_nodes_file)
-gdfPaveLink = gpd.read_file(pavement_links_file)
-gdfTopoPed = gpd.read_file(pavement_polygons_file)
+    gdfPaveNode = gpd.read_file(pavement_nodes_file)
+    gdfPaveLink = gpd.read_file(pavement_links_file)
+    gdfTopoPed = gpd.read_file(pavement_polygons_file)
 
-centre_poi_geom = gdf_pois.loc[ gdf_pois['ref_no'] == centre_poi_ref, 'geometry'].values[0]
-gdfTopoPed['dist_to_centre'] = gdfTopoPed['geometry'].distance(centre_poi_geom)
-centre_pavement_geometry = gdfTopoPed.sort_values(by='dist_to_centre', ascending=True)['geometry'].values[0]
+    centre_poi_geom = gdf_pois.loc[ gdf_pois['ref_no'] == centre_poi_ref, 'geometry'].values[0]
+    gdfTopoPed['dist_to_centre'] = gdfTopoPed['geometry'].distance(centre_poi_geom)
+    centre_pavement_geometry = gdfTopoPed.sort_values(by='dist_to_centre', ascending=True)['geometry'].values[0]
 
-Os = []
-Os.append(get_random_point_in_polygon(centre_pavement_geometry))
+    Os = []
+    Os.append(get_random_point_in_polygon(centre_pavement_geometry))
 
-# Select destination nodes randomly by finding random points in polygons, after filtering out polygons that don't have pavement nodes on them,
-# and that don't correspond to OR road links that are listed in the config file as links to exclude (these tend to be links at the edge of the study area where the data is a bit dodgy)
-gdfTopoPed = gdfTopoPed.loc[ ~gdfTopoPed['roadLinkID'].isin(config['or_links_exclude_ped_ods'])]
-gdfTopoPed = gpd.sjoin(gdfTopoPed, gdfPaveNode, op='intersects')
-candidates = gdfTopoPed.loc[ gdfTopoPed['dist_to_centre'] > dist_from_centre_threshold, 'polyID'].values
+    # Select destination nodes randomly by finding random points in polygons, after filtering out polygons that don't have pavement nodes on them,
+    # and that don't correspond to OR road links that are listed in the config file as links to exclude (these tend to be links at the edge of the study area where the data is a bit dodgy)
+    gdfTopoPed = gdfTopoPed.loc[ ~gdfTopoPed['roadLinkID'].isin(config['or_links_exclude_ped_ods'])]
+    gdfTopoPed = gpd.sjoin(gdfTopoPed, gdfPaveNode, op='intersects')
+    candidates = gdfTopoPed.loc[ gdfTopoPed['dist_to_centre'] > dist_from_centre_threshold, 'polyID'].values
 
-nDs = int(prop_random_ODs * len(candidates))
+    nDs = int(prop_random_ODs * len(candidates))
 
-# Choose random geoms, then choose random points in those geoms
-Ds = []
-while len(Ds)<nDs:
-    ri = np.random.randint(0, gdfTopoPed.shape[0])
-    pavement_geom = gdfTopoPed.iloc[ri]['geometry']
-    pavement_location = get_random_point_in_polygon(pavement_geom)
-    d = min(gdfPaveLink.distance(pavement_location))
-
-    # filter out any locations that are too far from a road link, but only try a few times before skipping this geometry
-    i = 0
-    while (d>min_distance_of_ped_od_to_ped_road_link) & (i<5):
+    # Choose random geoms, then choose random points in those geoms
+    Ds = []
+    while len(Ds)<nDs:
+        ri = np.random.randint(0, gdfTopoPed.shape[0])
+        pavement_geom = gdfTopoPed.iloc[ri]['geometry']
         pavement_location = get_random_point_in_polygon(pavement_geom)
         d = min(gdfPaveLink.distance(pavement_location))
-        i+=1
 
-    if d<min_distance_of_ped_od_to_ped_road_link:
-        Ds.append(pavement_location)
+        # filter out any locations that are too far from a road link, but only try a few times before skipping this geometry
+        i = 0
+        while (d>min_distance_of_ped_od_to_ped_road_link) & (i<5):
+            pavement_location = get_random_point_in_polygon(pavement_geom)
+            d = min(gdfPaveLink.distance(pavement_location))
+            i+=1
 
-ODs = Os+Ds
-inputFlows = [460] + [0]*len(Ds)
-data = {'fid': ['od_{}'.format(i) for i in range(len(ODs))], 'inFlow':inputFlows, 'geometry':ODs}
-gdfODs = gpd.GeoDataFrame(data, geometry = 'geometry')
-gdfODs.crs = projectCRS
+        if d<min_distance_of_ped_od_to_ped_road_link:
+            Ds.append(pavement_location)
 
-gdfODs.to_file(pedestrian_od_file)
+    ODs = Os+Ds
+    inputFlows = [460] + [0]*len(Ds)
+    data = {'fid': ['od_{}'.format(i) for i in range(len(ODs))], 'inFlow':inputFlows, 'geometry':ODs}
+    gdfODs = gpd.GeoDataFrame(data, geometry = 'geometry')
+    gdfODs.crs = projectCRS
 
-
-# Load ped ODs to get number of origins/destinations
-gdfODs = gpd.read_file(pedestrian_od_file)
+    gdfODs.to_file(pedestrian_od_file)
 
 #################
 #
@@ -126,7 +125,8 @@ gdfODs = gpd.read_file(pedestrian_od_file)
 #
 #################
 
-# Set distribution to draw flows from
+# Load ped ODs to get number of origins/destinations
+gdfODs = gpd.read_file(pedestrian_od_file)
 
 # Constants related to simulation run time and frequency of pedestrian addition
 T = 900
@@ -142,7 +142,8 @@ def get_flow(g, num_ods, total_in_flow, T=T, v = v, distribution = 'uniform'):
         return 0
 
 # Initialise OD flows matrix
-flows = np.zeros([len(ODs), len(ODs)])
+nODs = gdfODs.shape[0]
+flows = np.zeros([nODs, nODs])
 ODids = gdfODs['fid'].to_list()
 dfFlows = pd.DataFrame(flows, columns = ODids, index = ODids)
 
